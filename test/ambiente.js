e2bb42658588edfaf90183c54000d3687956e137
Robert Hurst <robertwhurst@gmail.com>
2013-10-25 09:57:53 -0700

    Finished ConfigLoader class and ambiente factory.

--- test/ambiente.js
+++ test/ambiente.js
@@ -1,9 +1,36 @@
 
+// modules
+var path = require('path');
+
+// libs
+var ambiente = require('../');
+var ConfigLoader = require('../lib/config-loader');
+var Config = require('../lib/config');
+
+// constants
+var CONFIG_PATH = path.resolve(__dirname, './fixtures/dev');
 
 describe('ambiente', function() {
 
-  it('accepts a config path', function() {
-    
+  it('wraps the config loader class', function() {
+    var config = ambiente(CONFIG_PATH);
+    config.should.be.an.instanceOf(ConfigLoader);
+  });
+
+});
+
+describe('ambiente.Config', function() {
+
+  it('references the Config class', function() {
+    ambiente.Config.should.equal(Config);
+  });
+
+});
+
+describe('ambiente.ConfigLoader', function() {
+  
+  it('references the ConfigLoader class', function() {
+    ambiente.ConfigLoader.should.equal(ConfigLoader);
   });
 
 });
--- test/config-loader.js
+++ test/config-loader.js
@@ -6,21 +6,38 @@ var path = require('path');
 var ConfigLoader = require('../lib/config-loader');
 
 // constants
-var APP_PATH = path.resolve(__dirname, 'fixtures/app');
-var DEV_PATH = path.resolve(__dirname, 'fixtures/dev');
+var CONFIG_PATH = path.resolve(__dirname, './fixtures/dev');
+var DEFAULTS_PATH = path.resolve(__dirname, './fixtures/app');
 
 describe('configLoader', function() {
 
-  it('accepts a config directory', function() {
-    new ConfigLoader(DEV_PATH);
+  it('accepts a config path', function() {
+    var config = new ConfigLoader(CONFIG_PATH);
   });
 
-  it('accepts a config directory, and a default config directory', function() {
-    new ConfigLoader(DEV_PATH, APP_PATH, function(err, configLoader) {
-      console.log(err, configLoader);
+  it('accepts a config path and defaults path', function() {
+    var config = new ConfigLoader(CONFIG_PATH, DEFAULTS_PATH);
+  });
+
+  it('accepts a config path, defaults path, and callback', function(done) {
+    var config = new ConfigLoader(CONFIG_PATH, DEFAULTS_PATH, done);
+  });
+
+  it('accepts a config path and callback', function(done) {
+    var config = new ConfigLoader(CONFIG_PATH, done);
+  });
+
+  it('sets the config namespaces on its instance', function() {
+    new ConfigLoader(CONFIG_PATH, function(err, config) {
+      if(err) { throw err; }
+      config.test.should.be.OK;
+      config.test.name.should.equal('prod');
+      config.test.a.should.OK;
+      config.test.a.foo.should.equal('bar');
+      config.namespace.should.be.OK;
+      config.namespace.subTest.should.be.OK;
+      config.namespace.subTest.sub.should.equal('val');
     });
   });
-
-
 
 });
